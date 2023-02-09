0af9476729a0f61a9f8e41afac90a6eb1f470d3a
Abrar Jahin <ajahin@dminc.com>
2023-02-09 14:56:32 -0500

    try 3

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="abrarjahin"
+export GITHUB_USERNAME="AbrarJahin"
 export GITHUB_REPO="bootcamp-2023-sls-api-jahin"
 export BUCKET_NAME="ajahin-bootcamp-2022-bucket" #username must be all lowercase
